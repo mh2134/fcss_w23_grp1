6147290a2acaed61a0eb637928e3db4af362d5a9
swekia <wepner@tugraz.at>
2024-02-02 17:36:03 +0100

    removed last unneccessary lines

--- code/regression.py
+++ code/regression.py
@@ -6,66 +6,12 @@ import colorcet as cc
 from pymer4.models import Lmer
 import pymer4.stats as pymers
 import numpy as np
-
-# axis limits for the final plots
 from fcss_w23_grp1.code.utils import *
-
-axlims = {
-    "ICCS0401": {  # robbery
-        "unemployment": {
-            "x0": -5, "x1": 1200, "y0": -30, "y1": 2300
-        },
-        "density": {
-            "x0": -70/10, "x1": 4200/10, "y0": -70, "y1": 2300
-        },
-        "gdp": {
-            "x0": 20, "x1": 400, "y0": -15, "y1": 2300
-        }
-    },
-    "ICCS050211": {  # theft
-        "unemployment": {
-            "x0": -20, "x1": 1200, "y0": -100, "y1": 5000
-        },
-        "density": {
-            "x0": -100/10, "x1": 4200/10, "y0": -100, "y1": 5000
-        },
-        "gdp": {
-            "x0": 20, "x1": 400, "y0": -100, "y1": 5000
-        }
-    },
-    "ICCS05012": {  # burglary
-        "unemployment": {
-            "x0": -22, "x1": 1200, "y0": 0, "y1": 7100
-        },
-        "density": {
-            "x0": -70/10, "x1": 4200/10, "y0": 0, "y1": 7100
-        },
-        "gdp": {
-            "x0": 20, "x1": 200, "y0": 0, "y1": 7100
-        }
-    },
-    "ICCS0101": {  # homicide
-        "unemployment": {
-            "x0": -12, "x1": 1200, "y0": -2, "y1": 40
-        },
-        "density": {
-            "x0": -2/10, "x1": 2700/10, "y0": -2, "y1": 40
-        },
-        "gdp": {
-            "x0": 20, "x1": 420, "y0": -2, "y1": 40
-        }
-    }
-}
 
 # read in preprocessed data
 df = pd.read_csv("data/normalized_data.csv")
 # rename colums
 df.rename(columns={"TIME_PERIOD": "year", "crime/population": "crimes", "unemp per Tsd.": "unemployment"}, inplace=True)
-# df.loc[:, "year"] = df["TIME_PERIOD"]
-# del df["TIME_PERIOD"]
-# df["crimes"] = df['crime/population']
-# df["unemployment"] = df['unemp per Tsd.']
-# throw NaN values in unemployment
 df = df[~df["unemployment"].isna()]
 
 # define the predictor variables
